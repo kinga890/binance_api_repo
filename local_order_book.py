b7bd147a0220525255f695bd3ebc13e154aa5cf0
kinga890 <fryckinga1@gmail.com>
2026-05-15 18:06:46 +0200

    setting local order book part2

--- local_order_book.py
+++ local_order_book.py
@@ -5,7 +5,6 @@ buffer = asyncio.Queue(1000)
 
 local_order_book = {}
 
-
 async def get_order_book_snapshot(client_from_main):
     order_book = await client_from_main.get_order_book(symbol='BTCUSDT', limit=1000)
     print(f"oder book: {order_book}")
@@ -23,7 +22,6 @@ async def get_order_book_snapshot(client_from_main):
 
     snapshot_ID = order_book.get('lastUpdateId')
     return snapshot_ID
-
 
 async def condition(client_from_main, depth_stream):
 
@@ -43,8 +41,30 @@ async def condition(client_from_main, depth_stream):
         websocket_U = package.get('U')
         websocket_u = package.get('u')
         if websocket_U<=snapshot_ID<=websocket_u:
-            print(f"local order book {local_order_book}")
-            print(websocket_U,snapshot_ID,websocket_u)
+            for x in package.get('b'):
+
+                if x[0] not in local_order_book['bids'].keys():  #new price appeared
+                    local_order_book['bids'].update({x[0] : x[1]})
+
+                if float(x[1]) > 0.0:   #quantity change
+                    local_order_book['bids'][x[0]] = x[1]
+
+                if float(x[1]) == 0.0:   #bid disappeared
+                    local_order_book['bids'].pop(x[0], None)
+
+            for y in package.get('a'):
+
+                if y[0] not in local_order_book['asks'].keys():  # new price appeared
+                    local_order_book['asks'].update({y[0]: y[1]})
+
+                if float(y[1]) > 0.0:  # quantity change
+                    local_order_book['asks'][y[0]] = y[1]
+
+                if float(y[1]) == 0.0:   #ask disappeared
+                    local_order_book['asks'].pop(y[0], None)
+
+            local_order_book['lastUpdateId'] = websocket_u
+
             break
         else:
             continue
@@ -65,6 +85,5 @@ async def main():
     await client_conn.close_connection()
 
 asyncio.run(main())
-
 
 
