93f1a89d691c6be91d32a0f444ae5c03fd0c35e0
kinga890 <fryckinga1@gmail.com>
2026-05-15 09:46:20 +0200

    using asyncio.Queue

--- local_order_book.py
+++ local_order_book.py
@@ -1,40 +1,49 @@
 import asyncio
 from binance import AsyncClient, BinanceSocketManager
 
+buffer = asyncio.Queue(1000)
+
+async def condition(client_from_main, depth_stream):
+
+    async def func():
+        async with depth_stream as stream:
+            while True:
+                res = await stream.recv()
+                await buffer.put(res)
+
+    task1 = asyncio.create_task(func())
+    await asyncio.sleep(3)
+
+    snapshot_ID = await get_order_book_snapshot(client_from_main)
+
+    while True:
+        package = await buffer.get()
+
+        websocket_U = package.get('U')
+        websocket_u = package.get('u')
+        if websocket_U<=snapshot_ID<=websocket_u:
+            print(websocket_U,snapshot_ID,websocket_u)
+            break
+        else:    #no need to delete the element, thanks do .get already gone
+            continue
+
 async def get_order_book_snapshot(client_from_main):
     order_book = await client_from_main.get_order_book(symbol='BTCUSDT', limit=1000)
     snapshot_ID = order_book.get('lastUpdateId')
     return snapshot_ID
 
-websocket_data = []
-
 async def get_websocket_data(client_from_main):
     websocket_conn = BinanceSocketManager(client=client_from_main)
-    depth_streams = websocket_conn.depth_socket(symbol='BTCUSDT')
-
-    async def func():
-        async with depth_streams as stream:
-            while True:
-                res = await stream.recv()
-                websocket_data.append(res)
-
-    task1 = asyncio.create_task(func())
-    await asyncio.sleep(3)
-    websocket_U = websocket_data[0]['U']
-    websocket_u = websocket_data[0]['u']
-
-    while True:
-        res1 = await get_order_book_snapshot(client_from_main)
-        if websocket_U > res1:
-            continue
-        else:
-            print(websocket_U,res1)
-            break
-
+    depth_stream = websocket_conn.depth_socket(symbol='BTCUSDT')
+    return depth_stream
 
 async def main():
     client_conn = await AsyncClient.create()
-    await get_websocket_data(client_conn)
+
+    taks_0 = await get_websocket_data(client_conn)
+    task_1 = condition(client_conn, taks_0)
+    await task_1
+
     await client_conn.close_connection()
 
 asyncio.run(main())
