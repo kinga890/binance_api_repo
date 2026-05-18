51b1cf0bbd28b1be2b09d70ae540f976abfd521f
kinga890 <fryckinga1@gmail.com>
2026-05-18 18:34:48 +0200

    after Kacper's review part 1

--- local_order_book.py
+++ local_order_book.py
@@ -1,7 +1,10 @@
 import asyncio
+import time
 from binance import AsyncClient, BinanceSocketManager, BinanceAPIException, BinanceRequestException
 import logging
 from logging.handlers import RotatingFileHandler
+
+
 
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[ RotatingFileHandler('logs/log.log', maxBytes=5 * 1024 * 1024, backupCount=5),
@@ -10,39 +13,46 @@ logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(levelname)s -
 buffer = asyncio.Queue()
 
 local_order_book = {
+    'E' : 0,
+    'data_obtained_time' : 0,
     'lastUpdateId': 0,
     'bids': {},
     'asks': {}
 }
 
 
-def apply_updates(package, websocket_u):
+
+def apply_updates(package, websocket_u,event_time, data_obtained_time):
     for x in package.get('b'):
-        if float(x[1]) > 0.0:  # quantity change or new price appeared
-            local_order_book['bids'][x[0]] = x[1]
+        bid_price = float(x[0])
+        bid_quantity = float(x[1])
+        if float(bid_quantity) > 0.0:  # quantity change or new price appeared
+            local_order_book['bids'][bid_price] = bid_quantity
         else:  # bid disappeared float(x[1]) == 0.0
-            local_order_book['bids'].pop(x[0], None)
+            local_order_book['bids'].pop(bid_price, None)
     for y in package.get('a'):
-        if float(y[1]) > 0.0:  # quantity change or new price appeared
-            local_order_book['asks'][y[0]] = y[1]
+        ask_price = float(y[0])
+        ask_quantity = float(y[1])
+        if float(ask_quantity) > 0.0:  # quantity change or new price appeared
+            local_order_book['asks'][ask_price] = ask_quantity
         else:  # ask disappeared: float(y[1]) == 0.0
-            local_order_book['asks'].pop(y[0], None)
-    local_order_book['lastUpdateId'] = int(websocket_u)
+            local_order_book['asks'].pop(ask_price, None)
+    local_order_book['E'] = event_time
+    local_order_book['data_obtained_time'] = data_obtained_time
+    local_order_book['lastUpdateId'] = websocket_u
 
 
-async def get_order_book_snapshot(client_from_main):
+async def get_order_book_snapshot(client_connection):
 
     while True:
         try:
-            order_book = await client_from_main.get_order_book(symbol='BTCUSDT', limit=1000)
+            order_book = await client_connection.get_order_book(symbol='BTCUSDT', limit=1000)
             local_order_book.update({
-                'lastUpdateId': order_book.get('lastUpdateId'),
-                'bids': dict(
-                    order_book.get('bids')
-                ),
-                'asks': dict(
-                    order_book.get('asks')
-                )
+                'E': int(time.time() * 1000),
+                'data_obtained_time': int(time.time() * 1000),
+                'lastUpdateId': int(order_book.get('lastUpdateId')),
+                'bids': {float(price) : float(quantity) for price,quantity in order_book.get('bids')},
+                'asks': {float(price) : float(quantity) for price,quantity in order_book.get('asks')}
             })
             snapshot_ID = int(order_book.get('lastUpdateId'))
             return snapshot_ID
@@ -54,10 +64,10 @@ async def get_order_book_snapshot(client_from_main):
             await asyncio.sleep(5)
 
 
-async def get_websocket_data(client_from_main):
-    websocket_conn = BinanceSocketManager(client=client_from_main)
-    depth_stream = websocket_conn.depth_socket(symbol='BTCUSDT')
-    return depth_stream
+async def create_websocket_tunnel(client_connection):
+    websocket_tunnel = BinanceSocketManager(client=client_connection)
+    websocket_stream = websocket_tunnel.depth_socket(symbol='BTCUSDT')
+    return websocket_stream
 
 
 async def first_update(snapshot_ID):
@@ -65,61 +75,69 @@ async def first_update(snapshot_ID):
         package = await buffer.get()
         websocket_U = int(package.get('U'))
         websocket_u = int(package.get('u'))
+        event_time = int(package.get('E'))
+        data_obtained_time = package['data_obtained_time']
         if websocket_U <= snapshot_ID <= websocket_u:
-            apply_updates(package, websocket_u)
+            apply_updates(package, websocket_u,event_time,data_obtained_time)
             break
         else:
             continue
 
-
-async def condition(client_from_main, depth_stream):
-
-    async def func():
-        while True:
-            try:
-                async with depth_stream as stream:
-                    while True:
-                        res = await asyncio.wait_for(stream.recv(), timeout= 10.0)
-                        await buffer.put(res)
-            except Exception as e:
-                logging.error('websocket error')
-                await asyncio.sleep(5)
-
-    task1 = asyncio.create_task(func())
-    await asyncio.sleep(3)
+async def fetch_websocket_data(websocket_stream):
     while True:
-        local_order_book.clear()
-        snapshot_ID = await get_order_book_snapshot(client_from_main)
-        await first_update(snapshot_ID)
-        while True:
-            package = await buffer.get()
-            websocket_U = int(package.get('U'))
-            websocket_u = int(package.get('u'))
-            current_lastUpdateId = int(local_order_book.get('lastUpdateId'))
-            if websocket_u < current_lastUpdateId:
-                continue
-            if websocket_U > (current_lastUpdateId + 1):
-                break
-            if websocket_U == (current_lastUpdateId + 1):
-                # subsequent updates
-                apply_updates(package, websocket_u)
+        try:
+            async with websocket_stream as stream:
+                while True:
+                    res = await asyncio.wait_for(stream.recv(), timeout= 10.0)
+                    res['data_obtained_time'] = int(time.time() * 1000)
+                    await buffer.put(res)
+        except Exception as e:
+            logging.error('websocket error')
+            await asyncio.sleep(5)
+
+
+async def condition(client_connection, websocket_stream):
+
+    asyncio.create_task(fetch_websocket_data(websocket_stream))
+
+    while True:
+        if buffer.qsize() != 0:
+            local_order_book.clear()
+            snapshot_ID = await get_order_book_snapshot(client_connection)
+            await first_update(snapshot_ID)
+            while True:
+                package = await buffer.get()
+                websocket_U = int(package.get('U'))
+                websocket_u = int(package.get('u'))
+                event_time = int(package.get('E'))
+                data_obtained_time = package['data_obtained_time']
+                current_id = local_order_book.get('lastUpdateId')
+                if websocket_u < current_id:
+                    continue
+                if websocket_U > (current_id + 1):
+                    break
+                if websocket_U == (current_id + 1):
+                    # subsequent updates
+                    apply_updates(package, websocket_u,event_time,data_obtained_time)
+        else:
+            await asyncio.sleep(3)
 
 
 async def main():
 
     while True:
         try:
-            client_conn = await AsyncClient.create()
+            client_connection = await AsyncClient.create()
             break
         except Exception as e:
             logging.error('client error')
             await asyncio.sleep(5)
     try:
-        taks_0 = await get_websocket_data(client_conn)
-        task_1 = condition(client_conn, taks_0)
-        await task_1
+        task_0 = await create_websocket_tunnel(client_connection)
+        await condition(client_connection, task_0)
+
     finally:
-        await client_conn.close_connection()
+        await client_connection.close_connection()
 
 
 if __name__ == "__main__":
